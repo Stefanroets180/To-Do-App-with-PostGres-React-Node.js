8cd8f2042a6d36a83918c8170e9281d833158e40
Stefan Roets <stefanroets@Stefans-MBP.mshome.net>
2023-02-13 00:49:44 +0200

    linking backend to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
-import ListHeader from "./components/ListHeader";
+import ListHeader from './components/ListHeader';
+import ListItem from './components/ListItem'
+import { useEffect, useState } from 'react'
 
 const App = () => {
+    const userEmail = 'stefan@test.com'
+    const [ tasks, setTasks] = useState(null)
+
+    const getData = async () => {
+        try {
+            const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+            const json =  await response.json()
+            setTasks(json)
+        }   catch (err) {
+            console.error(err)
+        }
+    }
+    useEffect(() => getData,[])
+
+    console.log(tasks)
+
+    //sort by date
+    const sortedTasks = tasks?.sort((a,b) => new Date(a.date) - new Date (b.date))
+
   return (
     <div className="app">
-      <ListHeader listName={'🏝Holiday tick list'}/>
+      <ListHeader listName={'🏝Holiday tick list'} />
+        {sortedTasks?.map((task) => <ListItem key={task.id} task={task} />)}
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,21 +1,19 @@
 const PORT = process.env.PORT ?? 8000
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const pool = require('./db')
 
-// app.get('/', (req, res) => {
-//    res.send('hello! Stefan')
-// })
+app.use(cors())
 
-app.get('/todos', async (req, res) => {
-
+// get all todos
+app.get('/todos/:userEmail', async (req, res) => {
+    const { userEmail } = req.params
     try {
-       const todos = await pool.query('SELECT * FROM todos')
+       const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
         res.json(todos.rows)
     } catch (err) {
         console.error(error)
     }
 })
-
-
-app.listen(PORT,()=> console.log(`Server running on PORT ${PORT}`))
+app.listen(PORT, ( )=> console.log(`Server running on PORT ${PORT}`))
