70fb3ec82b9a0eef9797d02e8de7440eb61909b8
Stefan Roets <stefanroets@Stefans-MBP.mshome.net>
2023-02-13 22:36:26 +0200

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import ListHeader from './components/ListHeader';
 import ListItem from './components/ListItem'
+import Auth from './components/Auth'
 import { useEffect, useState } from 'react'
 
 const App = () => {
     const userEmail = 'stefan@test.com'
     const [ tasks, setTasks] = useState(null)
 
+    const authToken = false
+
     const getData = async () => {
         try {
-            const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+            const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`)
             const json =  await response.json()
             setTasks(json)
         }   catch (err) {
@@ -24,8 +27,12 @@ const App = () => {
 
   return (
     <div className="app">
-      <ListHeader listName={'🏝Holiday tick list'} getData={getData} />
+        {!authToken && <Auth/>}
+        {authToken &&
+            <>
+        <ListHeader listName={'🏝Holiday tick list'} getData={getData} />
         {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
+            </>}
     </div>
   )
 }
