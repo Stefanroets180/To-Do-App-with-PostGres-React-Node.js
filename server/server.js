aafad452b8ca2d3b34b9dbe496de1c04f2ced86e
Stefan Roets <stefanroets@Stefans-MBP.mshome.net>
2023-02-13 20:29:04 +0200

    Delete todos

--- server/server.js
+++ server/server.js
@@ -33,5 +33,29 @@ app.post('/todos', async (req, res) =>{
     }
 })
 
+// Edit a todo
+app.put('/todos/:id', async (req, res) => {
+    const { id } = req.params
+    const {user_email, title, progress, date} = req.body
+    try {
+      const editToDO =
+          await pool.query('UPDATE todos SET user_email = $1, title = $2, progress = $3, date = $4 WHERE id = $5;',
+          [user_email, title, progress, date, id])
+        res.json(editToDO)
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+//Delete a todo
+app.delete('/todos/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const deleteToDo = await pool.query('DELETE FROM todos WHERE id = $1;', [id])
+        res.json(deleteToDo)
+    } catch (err) {
+        console.error(err)
+    }
+})
 
 app.listen(PORT, ( )=> console.log(`Server running on PORT ${PORT}`))
