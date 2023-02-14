f0090d657ac3967060b848b884c054b2b7a42f9d
Stefan Roets <stefanroets@Stefans-MBP.mshome.net>
2023-02-14 16:59:48 +0200

    jwt Token half-way

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -3,11 +3,32 @@ import { useState } from 'react'
 const Auth = () => {
     const [isLogIn, setIsLogin] = useState(true)
     const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confirmPassword, setConfirmPassword] = useState(null)
     const [error, setError] = useState(null)
+
+    console.log(email, password, confirmPassword)
 
     const viewLogin = (status) => {
         setError(null)
         setIsLogin(status)
+    }
+
+    const handleSubmit = async (e, endpoint) => {
+        e.preventDefault()
+        if (!isLogIn && password !== confirmPassword) {
+            setError('Make sure passwords match!')
+            return
+        }
+
+        const response = await fetch(`${process.env.REACT_APP_SERVERURL}/${endpoint}`,{
+            method: 'POST',
+            headers: { 'Content-Type' : 'application/json' },
+            body: JSON.stringify({ email, password })
+        })
+
+        const data = await response.json()
+        console.log(data)
     }
 
     return (
@@ -15,10 +36,10 @@ const Auth = () => {
             <div className="auth-container-box">
                 <form>
                     <h2>{isLogIn ? 'Please log in' : 'Plrase sign up'}</h2>
-                    <input type="email" placeholder="email"/>
-                    <input type="password" placeholder="password"/>
-                    {!isLogIn && <input type="password" placeholder="confirm password"/>}
-                    <input type="submit" className="create"/>
+                    <input type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
+                    {!isLogIn && <input type="password" placeholder="confirm password" onChange={(e) => setConfirmPassword(e.target.value)}/>}
+                    <input type="submit" className="create" onClick={(e) =>handleSubmit(e, isLogIn ? 'login' : 'signuo')}/>
                     {error && <p>{error}</p>}
                 </form>
                 <div className="auth-options">
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const { v4: uuidv4 } = require('uuid')
 const cors = require('cors')
 const app = express()
 const pool = require('./db')
+const bcrypt = require ('bcrypt')
+const jwt = require ('jsonwebtoken')
 
 app.use(cors())
 app.use(express.json())
@@ -53,6 +55,38 @@ app.delete('/todos/:id', async (req, res) => {
     try {
         const deleteToDo = await pool.query('DELETE FROM todos WHERE id = $1;', [id])
         res.json(deleteToDo)
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+// SignUP
+app.post('/signup', async (req, res) => {
+    const { email, password } = reg.body
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
+
+    try {
+        const signUp = await pool.query(`INSERT INTO users (email, hashed_password) VALUES ($1, $2)`,
+            [email, hashedPassword])
+
+        const token = jwt.sign({ email }, 'secret', { expiresIn: '1h'})
+
+        res.json({ email, token})
+
+    } catch (err) {
+        console.error(err)
+        if(err) {
+            res.json({detail: err.detail})
+        }
+    }
+})
+
+// Login
+app.post('/login', async (req, res) => {
+    const { email, password } = reg.body
+    try {
+
     } catch (err) {
         console.error(err)
     }
